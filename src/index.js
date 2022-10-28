0285114f1f41bd0b9518f235626c65ecf6fd6170
Justin Boker <justin@jumperstudios.com>
2022-10-28 13:54:10 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
 import NavBar from './components/navigation';
 import LoginForm from './components/login-form';
 import reportWebVitals from './reportWebVitals';
